c673988f4e1ee5322a11fbd52134330166800297
whacked-xps <whacked@users.noreply.github.com>
2021-11-20 10:04:07 +0800

    rename JsonSchema to JsonSchemaRecord (= database row)

--- src/autogen/interfaces/JsonSchemaRecord.ts
+++ src/autogen/interfaces/JsonSchemaRecord.ts
@@ -5,7 +5,7 @@
  * and run json-schema-to-typescript to regenerate this file.
  */
 
-export interface JsonSchemaSchema {
+export interface JsonSchemaRecordSchema {
   content: string;
   createdAt: string;
   description: string;
