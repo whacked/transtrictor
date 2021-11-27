ec41b44e41d222db7695d7c6c61051fb8574fc3e
whacked-xps <whacked@users.noreply.github.com>
2021-11-27 18:45:02 +0800

    add basic schema/data browser webapp

---
+++ app/webserver.ts
@@ -0,0 +1,37 @@
+import express from 'express'
+import ExpressPouchDb from 'express-pouchdb'
+import {
+    pouchDbConfig,
+    SchemaStatisticsLoader,
+} from '../src/docdb'
+
+
+const SERVER_PORT = 1235
+
+
+const ePdb = ExpressPouchDb(pouchDbConfig)
+const app = express()
+app.use('/api', ePdb)
+
+export function startWebserver() {
+    return app.listen(SERVER_PORT, () => {
+        console.log(`data server running on port ${SERVER_PORT}`)
+    })
+}
+
+if (require.main == module) {
+
+    let randomData = ['foo', 'bar', 'baz'].map((data, index) => {
+        let varyingStructure = index < 2
+            ? { oneLevel: 'flat' }
+            : { two: { level: 'nested' } }
+        return {
+            index,
+            myText: data,
+            ...varyingStructure,
+        }
+    })
+
+    SchemaStatisticsLoader.autoLoadData(randomData)
+    startWebserver()
+}
